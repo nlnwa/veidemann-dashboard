e8fc548981c8a5e55feda4f909eb967853326dde
Andreas Børsheim <andreas.borsheim@nb.no>
2017-10-12 10:48:35 +0200

    Add i18n to search component. Add text resoruce class to serve tooltip text

---
+++ src/app/search/search-text/search-text.ts
@@ -0,0 +1,14 @@
+import {OnInit} from '@angular/core';
+
+export class SearchText implements OnInit {
+
+addEntityTooltip  = 'En entitet besitter et eller flere nettsted.' + '\n' +
+  'Eks. VG (vg.no) eller Nasjonalbiblioteket (nb.no, ibsen.no, pliktavlevering.no)'
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
