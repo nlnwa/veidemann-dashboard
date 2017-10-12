f8f179ce3e91266a0b47933cd3bec57be664e8ce
Andreas Børsheim <andreas.borsheim@nb.no>
2017-10-12 15:01:15 +0200

    Small fix where i18n is used in components  - Use same i18n ID for all meta placeholders and errors  - Fix typos in id's and use the same format  - Add text resource class to crawlhostgroupconfig  - Add missing ID's

---
+++ src/app/configurations/crawlhostgroupconfig/crawlhostgroupconfig-text.ts
@@ -0,0 +1,13 @@
+import {OnInit} from '@angular/core';
+
+export class CrawlHostGroupConfigText implements OnInit {
+
+  descriptionTooltip = 'Legg til en fritekst om konfigurasjonen';
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
