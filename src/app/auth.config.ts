9b4157b3fa7402552c7c0e8d6003854ab0eaa6d9
John Erik Halse <johnerikhalse@gmail.com>
2017-10-16 16:49:51 +0200

    OpenID Connect authentication

---
+++ src/app/auth.config.ts
@@ -0,0 +1,19 @@
+import { AuthConfig } from 'angular-oauth2-oidc';
+
+export const authConfig: AuthConfig = {
+
+  // Url of the Identity Provider
+  issuer: 'http://$EXTERNAL_HOSTNAME:32000/dex',
+
+  // URL of the SPA to redirect the user to after login
+  redirectUri: window.location.origin + '/activity',
+
+  // The SPA's id. The SPA is registerd with this id at the auth-server
+  clientId: 'veidemann-admin',
+
+  // set the scope for the permissions the client should request
+  // The first three are defined by OIDC. The 4th is a usecase-specific one
+  scope: 'openid profile email groups',
+
+  requireHttps: false
+}
