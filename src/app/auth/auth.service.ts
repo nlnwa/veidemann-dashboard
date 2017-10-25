7817155e9e289c05556ce81143f63a1c03111284
Marius Elsfjordstrand Beck <marius.beck@nb.no>
2017-10-25 10:44:11 +0200

    Implement interceptor that sends bearer token in HTTP header when calling backend
    - Refactor auth code into AuthService
    - Implement HTTP interceptor

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import {JwksValidationHandler, OAuthService} from 'angular-oauth2-oidc';
+import {authConfig} from './auth.config';
+
+@Injectable()
+export class AuthService {
+  constructor(private oauthService: OAuthService) {
+    this.configureAuth();
+  }
+
+  public get groups() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    } else {
+      return claims['groups'];
+    }
+  }
+
+  public get name() {
+    const claims = this.oauthService.getIdentityClaims();
+    if (!claims) {
+      return null;
+    } else {
+      return claims['name'];
+    }
+  }
+
+  public get authorizationHeader() {
+      return this.oauthService.authorizationHeader();
+  }
+
+  public login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  public logout() {
+    this.oauthService.logOut();
+  }
+
+  private configureAuth() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+}
