6a432cca3ddae6fa6864f6193f18976f1259ea53
Marius Elsfjordstrand Beck <marius.beck@nb.no>
2017-10-26 14:46:59 +0200

    Change clientId in auth config. Fix typo.

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -8,8 +8,8 @@ export const authConfig: AuthConfig = {
   // URL of the SPA to redirect the user to after login
   redirectUri: window.location.origin + '/activity',
 
-  // The SPA's id. The SPA is registerd with this id at the auth-server
-  clientId: 'veidemann-admin',
+  // The SPA's id. The SPA is registered with this id at the auth-server
+  clientId: 'veidemann-api',
 
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
