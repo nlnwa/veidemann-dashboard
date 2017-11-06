622afba89853528d5fbbb32a41c4e8a3c2382d79
Andreas Børsheim <andreas.borsheim@nb.no>
2017-11-06 08:58:30 +0100

    Refactor politenessconfig to use the common code style and naming

--- src/app/configurations/politenessconfig/politenessconfig-details/politenessconfig-details.component.ts
+++ src/app/configurations/politenessconfig/politenessconfig-details/politenessconfig-details.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CustomValidators} from '../../../commons/';
 import {isUndefined} from 'util';
@@ -14,31 +14,107 @@ import {SnackBarService} from '../../../snack-bar-service/snack-bar.service';
 })
 
 export class PolitenessconfigDetailsComponent implements OnChanges {
+
   @Input()
   politenessConfig: PolitenessConfig;
-  @Input()
-  createHandler: Function;
-  @Input()
-  updateHandler: Function;
-  @Input()
-  deleteHandler: Function;
+
+  @Output()
+  created = new EventEmitter<PolitenessConfig>();
+  @Output()
+  updated = new EventEmitter<PolitenessConfig>();
+  @Output()
+  deleted = new EventEmitter<PolitenessConfig>();
 
   form: FormGroup;
 
   robotsPolicyList: any = [];
-  robetsPolicyDropdownSettings = {};
   selectedRobotsPolicyItems = [];
+  robotsPolicyDropdownSettings = {
+    singleSelection: true,
+    text: 'Velg Robots policy',
+  };
+
 
   constructor(private politenessConfigService: PolitenessConfigService,
               private snackBarService: SnackBarService,
               private fb: FormBuilder) {
-    this.filldropdown();
+    this.fillDropdown();
     this.createForm();
+  }
+
+  get name() {
+    return this.form.get('meta.name');
+  }
+
+  get robotsPolicy() {
+    return this.form.get('robots_policy');
+  }
+
+  get customRobots() {
+    return this.form.get('custom_robots');
+  }
+
+  get minRobotsValidDurationSec() {
+    return this.form.get('minimum_robots_validity_duration_s');
+  }
+
+  get minTimeBetweenPageloadMs() {
+    return this.form.get('min_time_between_page_load_ms');
+  }
+
+  get maxTimeBetweenPageloadMs() {
+    return this.form.get('max_time_between_page_load_ms');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (!this.politenessConfig.crawl_host_group_selector) {
+      this.politenessConfig.crawl_host_group_selector = new Selector();
+    }
+    if (changes.politenessConfig.currentValue) {
+      this.updateForm();
+    }
+  }
+
+  onSave() {
+    this.politenessConfig = this.prepareSave();
+    this.politenessConfigService.create(this.politenessConfig)
+      .subscribe(newPolitenessConfig => {
+        this.politenessConfig = newPolitenessConfig;
+        this.updateForm();
+        this.created.emit(newPolitenessConfig);
+        this.snackBarService.openSnackBar('Lagret');
+      });
+  }
+
+  onUpdate(): void {
+    this.politenessConfig = this.prepareSave();
+    this.politenessConfigService.update(this.politenessConfig)
+      .subscribe(updatedPolitenessConfig => {
+        this.politenessConfig = updatedPolitenessConfig;
+        this.updateForm();
+        this.updated.emit(updatedPolitenessConfig);
+        this.snackBarService.openSnackBar('Lagret');
+      });
+  }
+
+  onDelete() {
+    this.politenessConfigService.delete(this.politenessConfig.id)
+      .subscribe((response) => {
+        this.deleted.emit(this.politenessConfig);
+        this.politenessConfig = response;
+        this.form.reset();
+        this.snackBarService.openSnackBar('Slettet');
+      });
+  }
+
+  onRevert() {
+    this.updateForm();
+    this.snackBarService.openSnackBar('Tilbakestilt');
   }
 
   private createForm() {
     this.form = this.fb.group({
-      id: '',
+      id: {value: '', disabled: true},
       robots_policy: ['', [Validators.required, CustomValidators.nonEmpty]],
       minimum_robots_validity_duration_s: ['', [Validators.required, CustomValidators.min(0)]],
       custom_robots: null,
@@ -56,101 +132,32 @@ export class PolitenessconfigDetailsComponent implements OnChanges {
     });
   }
 
-  updateData(politenessconfig: PolitenessConfig) {
-    this.form.controls['id'].setValue(politenessconfig.id);
-    this.form.controls['minimum_robots_validity_duration_s'].setValue(politenessconfig.minimum_robots_validity_duration_s);
-    this.form.controls['custom_robots'].setValue(politenessconfig.custom_robots);
-    this.form.controls['min_time_between_page_load_ms'].setValue(politenessconfig.min_time_between_page_load_ms);
-    this.form.controls['max_time_between_page_load_ms'].setValue(politenessconfig.max_time_between_page_load_ms);
-    this.form.controls['delay_factor'].setValue(politenessconfig.delay_factor);
-    this.form.controls['max_retries'].setValue(politenessconfig.max_retries);
-    this.form.controls['retry_delay_seconds'].setValue(politenessconfig.retry_delay_seconds);
-    this.form.controls['meta'].patchValue({
-      name: politenessconfig.meta.name as string,
-      description: politenessconfig.meta.description as string,
-      label: [...politenessconfig.meta.label],
+  private updateForm() {
+    this.form.patchValue({
+      id: this.politenessConfig.id,
+      minimum_robots_validity_duration_s: this.politenessConfig.minimum_robots_validity_duration_s,
+      custom_robots: this.politenessConfig.custom_robots,
+      min_time_between_page_load_ms: this.politenessConfig.min_time_between_page_load_ms,
+      max_time_between_page_load_ms: this.politenessConfig.max_time_between_page_load_ms,
+      delay_factor: this.politenessConfig.delay_factor,
+      max_retries: this.politenessConfig.max_retries,
+      retry_delay_seconds: this.politenessConfig.retry_delay_seconds,
+      crawl_host_group_selector: [...this.politenessConfig.crawl_host_group_selector.label],
+      meta: {
+        name: this.politenessConfig.meta.name,
+        description: this.politenessConfig.meta.description,
+        label: [...this.politenessConfig.meta.label],
+      },
     });
-    this.form.get('crawl_host_group_selector').setValue([...politenessconfig.crawl_host_group_selector.label]);
     this.setSelectedDropdown();
-    this.selectedRobotsPolicyItems = [];
     this.form.markAsPristine();
+    this.form.markAsUntouched();
   };
 
-  ngOnChanges() {
-    if (!this.politenessConfig.crawl_host_group_selector) {
-      this.politenessConfig.crawl_host_group_selector = new Selector();
-    }
-    this.updateData(this.politenessConfig);
-  }
-
-  createPolitenessconfig() {
-    this.politenessConfig = this.prepareSavePolitenessConfig();
-    this.politenessConfigService.create(this.politenessConfig)
-      .subscribe((newPolitenessConfig: PolitenessConfig) => {
-        this.createHandler(newPolitenessConfig);
-      });
-    this.snackBarService.openSnackBar('Lagret');
-  }
-
-
-  updatePolitenessconfig(politenessConfig: PolitenessConfig): void {
-    this.politenessConfig = this.prepareSavePolitenessConfig();
-    this.politenessConfigService.update(this.politenessConfig)
-      .subscribe((updatedPolitenessConfig: PolitenessConfig) => {
-        this.updateHandler(updatedPolitenessConfig);
-      });
-    this.snackBarService.openSnackBar('Lagret');
-  }
-
-  deletePolitenessconfig(politenessConfigId) {
-    this.politenessConfigService.delete(politenessConfigId)
-      .subscribe((response) => {
-        this.deleteHandler(politenessConfigId);
-        if (response instanceof Object) {
-          this.snackBarService.openSnackBar('Feil: Ikke slettet');
-        } else {
-          this.snackBarService.openSnackBar('Slettet');
-        }
-      });
-  }
-
-  filldropdown() {
-    this.politenessConfigService.getRobotsConfig()
-      .subscribe(robotsPolicies => {
-        robotsPolicies.forEach((robotsPolicy) => {
-          this.robotsPolicyList.push(robotsPolicy);
-        });
-      });
-  }
-
-  setSelectedDropdown() {
-    if (!isUndefined(this.politenessConfig.id)) {
-      this.politenessConfigService.get(this.politenessConfig.id)
-        .subscribe(politenessConfig => {
-          for (const robotsPolicy of this.robotsPolicyList) {
-            if (robotsPolicy.itemName === politenessConfig.robots_policy) {
-              this.selectedRobotsPolicyItems.push({id: robotsPolicy.id, itemName: politenessConfig.robots_policy})
-            }
-          }
-          this.form.controls['robots_policy'].setValue(this.selectedRobotsPolicyItems);
-        });
-    }
-    this.robetsPolicyDropdownSettings = {
-      singleSelection: true,
-      text: 'Velg Robots policy',
-    };
-
-  }
-
-  prepareSavePolitenessConfig(): PolitenessConfig {
+  private prepareSave(): PolitenessConfig {
     const formModel = this.form.value;
-
-    // deep copy of form model lairs
     const labelsDeepCopy = formModel.meta.label.map(label => ({...label}));
     const crawlHostGroup_selectorDeepCopy: Selector = {label: formModel.crawl_host_group_selector.map(label => ({...label}))};
-
-    // return new `Hero` object containing a combination of original hero value(s)
-    // and deep copies of changed form model values
     return {
       id: this.politenessConfig.id,
       robots_policy: formModel.robots_policy[0].itemName,
@@ -174,8 +181,31 @@ export class PolitenessconfigDetailsComponent implements OnChanges {
     };
   }
 
-  revert() {
-    this.ngOnChanges();
-    this.snackBarService.openSnackBar('Tilbakestilt');
+  private fillDropdown() {
+    this.politenessConfigService.getRobotsConfig()
+      .subscribe(robotsPolicies => {
+        robotsPolicies.forEach((robotsPolicy) => {
+          this.robotsPolicyList.push(robotsPolicy);
+        });
+      });
   }
+
+  private setSelectedDropdown() {
+    this.selectedRobotsPolicyItems = [];
+    if (!isUndefined(this.politenessConfig.id)) {
+      this.politenessConfigService.get(this.politenessConfig.id)
+        .subscribe(politenessConfig => {
+          for (const robotsPolicy of this.robotsPolicyList) {
+            if (robotsPolicy.itemName === politenessConfig.robots_policy) {
+              this.selectedRobotsPolicyItems.push({
+                id: robotsPolicy.id,
+                itemName: politenessConfig.robots_policy})
+            }
+          }
+          this.robotsPolicy.setValue(this.selectedRobotsPolicyItems);
+        });
+    }
+  }
+
+
 }
