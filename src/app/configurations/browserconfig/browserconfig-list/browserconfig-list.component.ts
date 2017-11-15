508bb5a9287d4c904a270198596ae11c0a633b35
Andreas Børsheim <andreas.borsheim@nb.no>
2017-11-15 12:12:59 +0100

    Refactor browserconfig to use the new list

--- src/app/configurations/browserconfig/browserconfig-list/browserconfig-list.component.ts
+++ src/app/configurations/browserconfig/browserconfig-list/browserconfig-list.component.ts
@@ -1,73 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {BrowserConfigService} from '../browserconfig.service';
-import {BrowserConfig} from '../../../commons/models/config.model';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
+import {BaseListComponent} from '../../../commons/list/base-list/base-list.component';
+import {ListDataSource} from '../../../commons/list/list-datasource';
 
 @Component({
   selector: 'app-browserconfig-list',
   templateUrl: './browserconfig-list.component.html',
-  styleUrls: ['./browserconfig-list.component.css']
+  styleUrls: [
+    '../../../commons/list/base-list/base-list.component.css',
+    './browserconfig-list.component.css'
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class BrowserConfigListComponent implements OnInit {
 
-  browserConfigs: BrowserConfig[];
-  selectedBrowserConfig: BrowserConfig;
-
-  constructor(private browserConfigService: BrowserConfigService) {}
-
-  onBrowserConfigCreated(browserConfig: BrowserConfig) {
-    this.browserConfigs.push(browserConfig);
-    this.selectBrowserConfig(browserConfig);
-    return this.browserConfigs;
-  }
-
-  ngOnInit() {
-    this.browserConfigService.list()
-      .map(reply => reply.value)
-      .subscribe(browserConfigs => this.browserConfigs = browserConfigs);
-  }
-
-  onBrowserConfigDeleted(browserConfig: BrowserConfig) {
-    const idx = this.getIndexOfBrowserConfig(browserConfig.id);
-    if (idx !== -1) {
-      this.browserConfigs.splice(idx, 1);
-      this.selectBrowserConfig(null);
-    }
-    return this.browserConfigs
-  }
-
-  onBrowserConfigUpdated(browserConfig: BrowserConfig) {
-    const idx = this.getIndexOfBrowserConfig(browserConfig.id);
-    if (idx !== -1) {
-      this.browserConfigs[idx] = browserConfig;
-    }
-    return this.browserConfigs;
-  }
-
-  onCreateNewBrowserConfig() {
-    const browserConfig: BrowserConfig = {
-      user_agent: '',
-      page_load_timeout_ms: '',
-      sleep_after_pageload_ms: '',
-      script_selector: {
-        label: []
-      },
-      meta: {
-        name: '',
-        description: '',
-        label: [],
-      },
-    };
-    // By default, a newly-created  will have the selected state.
-    this.selectBrowserConfig(browserConfig);
-  }
-
-  private getIndexOfBrowserConfig(browserConfigId: String) {
-    return this.browserConfigs.findIndex((browserconfig) => {
-      return browserconfig.id === browserConfigId;
-    });
-  }
-
-  private selectBrowserConfig(browserConfig: BrowserConfig) {
-    this.selectedBrowserConfig = browserConfig
+export class BrowserConfigListComponent extends BaseListComponent {
+  constructor(public dataSource: ListDataSource,
+              protected changeDetectorRef: ChangeDetectorRef) {
+    super(dataSource, changeDetectorRef);
+    this.displayedColumns = ['name', 'description'];
   }
 }
+
