05be0bfb7611b3ae27946a87236369f3e5cedd21
Marius Elsfjordstrand Beck <marius.beck@nb.no>
2017-11-15 13:23:18 +0100

    Separate list of entities for search results

--- src/app/search/search-database.ts
+++ src/app/search/search-database.ts
@@ -1,10 +1,35 @@
 import {Injectable} from '@angular/core';
-import {Item, ListDatabase} from '../commons/list/list-database';
+import {Item, ListDatabase} from '../commons/list/';
+import {MatPaginator} from '@angular/material';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+import {empty} from 'rxjs/observable/empty';
+import 'rxjs/add/operator/combineLatest';
+import {Subscription} from 'rxjs/Subscription';
 
 @Injectable()
 export class SearchDatabase extends ListDatabase {
+  private pager: MatPaginator;
+  private changeSubscription: Subscription;
+  private _data: BehaviorSubject<Item[]>;
 
-  set items(items: Item[]) {
+  constructor() {
+    super();
+    this._data = new BehaviorSubject<Item[]>([]);
+    this.updateChangeSubscription();
+  }
+
+  get paginator(): MatPaginator {
+    return this.pager;
+  }
+
+  set paginator(paginator: MatPaginator) {
+    this.pager = paginator;
+    this.updateChangeSubscription();
+  }
+
+  get (): Item[] { return this._data.value; }
+
+  set data(items: Item[]) {
     if (items.length > 0) {
       this.dataSet.clear();
       // items can contain two or more of the same item with different type
@@ -19,27 +44,29 @@ export class SearchDatabase extends ListDatabase {
         }
         return acc;
       }, []);
-      this.dataChange.next(value);
+      this._data.next(value);
     } else {
-      this.dataChange.next(items);
+      this._data.next(items);
     }
   }
 
-  add(item: Item): void {
-    if (this.dataSet.has(item.id)) {
-      this.update(item);
-    } else {
-      const copy = this.items.slice();
-      copy.push(item);
-      this.items = copy;
+  private updateChangeSubscription() {
+    const pageChange = this.pager ? this.pager.page : empty();
+
+    if (this.changeSubscription) {
+      this.changeSubscription.unsubscribe();
     }
+
+    this.changeSubscription = this._data
+      .combineLatest(pageChange.startWith(null), (data, page) => data)
+      .map((data) => this.pageData(data))
+      .subscribe((data) => this.dataChange.next(data));
   }
 
-  update(item: Item): void {
-    const index = this.findIndex(item);
-    /* tslint:disable:no-bitwise */
-    (<any>this.items[index]).type &= (<any>item).type;
-    this.items = this.items.slice();
+  private pageData(data: Item[]): Item[] {
+    if (!this.pager) { return data; }
+    const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+    return data.slice().splice(startIndex, this.paginator.pageSize);
   }
 
 }
