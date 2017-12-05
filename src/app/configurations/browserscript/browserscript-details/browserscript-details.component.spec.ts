d9f4956ade6884024e50cc0fc7bbbe319500c4f7
Andreas Børsheim <andreas.borsheim@nb.no>
2017-12-05 13:56:55 +0100

    Fix how browserscript mock is created

--- src/app/configurations/browserscript/browserscript-details/browserscript-details.component.spec.ts
+++ src/app/configurations/browserscript/browserscript-details/browserscript-details.component.spec.ts
@@ -3,14 +3,13 @@ import {BrowserScriptDetailsComponent} from './browserscript-details.component';
 import {BrowserScript, Label} from '../../../commons/models/config.model';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {NO_ERRORS_SCHEMA, SimpleChange} from '@angular/core';
-import {DateTime} from '../../../commons/datetime';
-import {AceEditorModule} from "ng2-ace-editor";
+import {AceEditorModule} from 'ng2-ace-editor';
 
 describe('BrowserScriptDetailsComponent', () => {
   let component: BrowserScriptDetailsComponent;
   let fixture: ComponentFixture<BrowserScriptDetailsComponent>;
-  let expectedBrowserScript;
-  let expectedLabel;
+  let expectedBrowserScript: BrowserScript;
+  let expectedLabel: Label;
 
   // Async beforeEach needed when using external template
   beforeEach(async(() => {
@@ -27,36 +26,27 @@ describe('BrowserScriptDetailsComponent', () => {
     fixture = TestBed.createComponent(BrowserScriptDetailsComponent);
     component = fixture.componentInstance;
 
-
-    // Pretend that it was wired to something that supplied a browserscript
-    expectedBrowserScript = new BrowserScript();
-    expectedLabel = new Label();
-
-    // meta.label mock
     expectedLabel = {
-      type: 'Test',
+      key: 'Test',
       value: 'test',
     };
 
-    // BrowserScript mock
     expectedBrowserScript = {
       id: '1000',
       script: 'var test=5; console.log(test)',
       meta: {
         name: 'Test',
         description: 'Dette er en test',
-        created: DateTime.convertFullTimestamp(1511964561),
+        created: {seconds: '1511964561'},
         created_by: 'admin',
-        last_modified: DateTime.convertFullTimestamp(1511964561),
+        last_modified: {seconds: '1511964561'},
         last_modified_by: 'admin',
         label: [expectedLabel]
       }
     };
 
-    // Setting the @Input for the component to be our mocked  browserscript object
     component.browserScript = expectedBrowserScript;
 
-    // Triggering ngOnchanges which in turn calls updateForm() and fills the form
     component.ngOnChanges({
       browserScript: new SimpleChange(null, component.browserScript, null)
     });
@@ -64,7 +54,6 @@ describe('BrowserScriptDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  // Tests
   it('should create', () => {
     expect(component).toBeTruthy();
   });
@@ -107,19 +96,16 @@ describe('BrowserScriptDetailsComponent', () => {
         name: 'E',
       }
     });
-    // Check that form is invalid after update
+
     expect(component.form.valid).toBe(false);
 
-    // Update the form with a valid value
     component.form.patchValue({
       meta: {
         name: 'Endret navn',
       }
     });
     expect(component.form.valid).toBe(true);
-    // check that the form is valid after update
 
-    // Subscribe to the output of the component and check that emitted values are the same as the ones in the form
     component.update.subscribe((browserScriptValue: BrowserScript) => {
       expect(browserScriptValue.id).toBe(expectedBrowserScript.id);
       expect(browserScriptValue.script).toBe(expectedBrowserScript.script);
@@ -129,7 +115,6 @@ describe('BrowserScriptDetailsComponent', () => {
       done();
     });
 
-    // trigger the update function
     component.onUpdate();
   });
 
