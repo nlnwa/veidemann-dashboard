54fb3bb35659d6dda43719e5abef950c5a39adfe
Andreas Børsheim <andreas.borsheim@nb.no>
2018-02-08 14:03:15 +0100

    Store result of getRoles in service to avoid async calls. Add methods for checking for different roles

--- src/app/roles/roles.service.ts
+++ src/app/roles/roles.service.ts
@@ -2,28 +2,50 @@ import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Role, RoleList} from '../commons/models/config.model';
-import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class RoleService {
 
   private readonly URL: string = `${environment.apiGateway}/roles`;
 
+  private roles: Role[] = [Role.ANY];
+
   constructor(private http: HttpClient) {
   }
 
-  public getRoles(): Observable<Role[]> {
-    return this.http.get<RoleList>(this.URL)
-      .map(res => res.role.map(role => Role[role]));
+  public resetRoles() {
+    this.roles = [Role.ANY]
   }
 
-  public isAdmin(): Observable<boolean> {
-    return this.getRoles().map(roles => {
-      if (roles.includes(Role.ADMIN)) {
-        return true
-      } else {
-        return false;
-      }
-    });
+  public fetchRoles() {
+    this.http.get<RoleList>(this.URL)
+      .map(res => res.role.map(role => Role[role]))
+      .subscribe(roles => {
+        this.roles = roles;
+      });
+  }
+
+  public getRoles(): Role[] {
+    return this.roles;
+  }
+
+  public isAdmin(): boolean {
+    return this.isRole(Role.ADMIN)
+  }
+
+  public isCurator(): boolean {
+    return this.isRole(Role.CURATOR)
+  }
+
+  public isReadonly(): boolean {
+    return this.isRole(Role.READONLY)
+  }
+
+  private isRole(role: Role) {
+    if (this.roles.includes(role)) {
+      return true
+    } else {
+      return false;
+    }
   }
 }
