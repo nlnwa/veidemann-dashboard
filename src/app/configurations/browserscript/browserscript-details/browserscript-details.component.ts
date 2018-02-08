478381d4f50d432035b55fd9ce6d9db7d2d4a2a9
Andreas Børsheim <andreas.borsheim@nb.no>
2018-02-08 14:33:28 +0100

    Disable user input from users without qualified roles in browserscripts

--- src/app/configurations/browserscript/browserscript-details/browserscript-details.component.ts
+++ src/app/configurations/browserscript/browserscript-details/browserscript-details.component.ts
@@ -17,6 +17,7 @@ import 'brace/theme/monokai';
 import 'brace/mode/javascript';
 import 'brace/ext/language_tools.js';
 import {DateTime} from '../../../commons/datetime';
+import {RoleService} from '../../../roles/roles.service';
 
 declare var ace: any;
 
@@ -43,8 +44,14 @@ export class BrowserScriptDetailsComponent implements OnChanges, AfterViewInit {
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder,
+              private roleService: RoleService) {
     this.createForm();
+  }
+
+
+  get isAdmin(): boolean {
+    return this.roleService.isAdmin();
   }
 
   get showSave(): boolean {
@@ -123,21 +130,24 @@ export class BrowserScriptDetailsComponent implements OnChanges, AfterViewInit {
   }
 
   private createForm() {
+
+    const hasRequiredRole = !(this.roleService.isAdmin());
+
     this.form = this.fb.group({
       id: {value: '', disabled: true},
-      script: '',
+      script: {value: '', disabled: hasRequiredRole},
       meta: this.fb.group({
-        name: ['', [Validators.required, Validators.minLength(2)]],
-        description: '',
+        name: [{value: '', disabled: hasRequiredRole}, [Validators.required, Validators.minLength(2)]],
+        description: {value: '', disabled: hasRequiredRole},
         created: this.fb.group({seconds: {value: '', disabled: true}}),
         created_by: {value: '', disabled: true},
         last_modified: this.fb.group({seconds: {value: '', disabled: true}}),
         last_modified_by: {value: '', disabled: true},
-        label: [],
+        label: {value: [], disabled: hasRequiredRole},
       }),
     });
   }
-  1511964561
+
   private updateForm() {
     this.form.patchValue({
       id: this.browserScript.id,
