5f0b10d7b399f3531f2b546b6fbcd3ed94f56729
Andreas Børsheim <andreas.borsheim@nb.no>
2018-04-12 15:53:57 +0200

    fix: use correct name for component on import

--- src/app/configurations/warcstatus/warcstatus-list/warcstatus-list.component.spec.ts
+++ src/app/configurations/warcstatus/warcstatus-list/warcstatus-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { WarcstatusListComponent } from './warcstatus-list.component';
+import { WarcStatusListComponent } from './warcstatus-list.component';
 
 describe('WarcstatusListComponent', () => {
-  let component: WarcstatusListComponent;
-  let fixture: ComponentFixture<WarcstatusListComponent>;
+  let component: WarcStatusListComponent;
+  let fixture: ComponentFixture<WarcStatusListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WarcstatusListComponent ]
+      declarations: [ WarcStatusListComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(WarcstatusListComponent);
+    fixture = TestBed.createComponent(WarcStatusListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
