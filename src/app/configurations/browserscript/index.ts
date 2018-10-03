ba50d0b53d9d41c9034b763976d2c70bce720087
Andreas Børsheim <andreas.borsheim@nb.no>
2018-10-03 13:54:04 +0200

    fix: add export to browserscript index. Remove imports for deleted list components

--- src/app/configurations/browserscript/index.ts
+++ src/app/configurations/browserscript/index.ts
@@ -1,3 +1,4 @@
 export {BrowserScriptService} from './browserscript.service';
 export {BrowserScriptDetailsComponent} from './browserscript-details/browserscript-details.component';
 export {BrowserScriptPageComponent} from './browserscript-page.component';
+export {BrowserScriptDirective} from './browserscript-details/browserscript.directive';
--- src/app/configurations/configurations.module.ts
+++ src/app/configurations/configurations.module.ts
@@ -2,61 +2,80 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 
 import {CommonsModule} from '../commons/commons.module';
 
+import {SearchConfigComponent} from './search/search-config/search-config.component';
+
+import {DetailDirective} from './shared/detail.directive';
+
 import {
   CrawlConfigDetailsComponent,
-  CrawlConfigListComponent,
   CrawlConfigPageComponent,
   CrawlConfigService
 } from './crawlconfig';
 import {
   BrowserScriptDetailsComponent,
-  BrowserScriptListComponent,
   BrowserScriptPageComponent,
-  BrowserScriptService
+  BrowserScriptService,
+  BrowserScriptDirective
 } from './browserscript';
-import {SearchComponent, SearchListComponent, SearchService} from './search';
-import {CrawljobDetailsComponent, CrawlJobListComponent, CrawlJobsComponent, CrawlJobService} from './crawljobs';
+import {
+  SearchComponent,
+  SearchListComponent,
+  SearchService
+} from './search';
+import {
+  CrawljobDetailsComponent,
+  CrawlJobsComponent,
+  CrawlJobService
+} from './crawljobs';
 import {
   PolitenessconfigDetailsComponent,
-  PolitenessConfigListComponent,
   PolitenessConfigPageComponent,
   PolitenessConfigService
 } from './politenessconfig';
 import {
   CrawlHostGroupConfigDetailsComponent,
-  CrawlHostGroupConfigListComponent,
   CrawlHostGroupConfigPageComponent,
   CrawlHostGroupConfigService
 } from './crawlhostgroupconfig';
 import {
   ScheduleDetailsComponent,
-  ScheduleListComponent,
   SchedulePageComponent,
   ScheduleService,
   ScheduleSidebarComponent
 } from './schedule';
-import {EntityDetailsComponent, EntityListComponent, EntityService} from './entities';
-import {LoglevelComponent, LogService} from './logs';
+import {
+  EntityDetailsComponent,
+  EntityListComponent,
+  EntityService
+} from './entities';
+import {
+  LoglevelComponent,
+  LogService
+} from './logs';
 import {
   BrowserConfigDetailsComponent,
-  BrowserConfigListComponent,
   BrowserConfigPageComponent,
   BrowserConfigService
 } from './browserconfig';
-import {SeedDetailComponent, SeedListComponent, SeedService} from './seeds';
+import {
+  SeedDetailComponent,
+  SeedListComponent,
+  SeedService
+} from './seeds';
 import {
   RoleMappingDetailsComponent,
   RoleMappingListComponent,
   RoleMappingPageComponent,
   RoleMappingService
 } from './rolemapping';
-import {BrowserScriptDirective} from './browserscript/browserscript-details/browserscript.directive';
-import {WarcStatusListComponent} from './warcstatus/warcstatus-list/warcstatus-list.component';
-import {WarcStatusDetailsComponent} from './warcstatus/warcstatus-details/warcstatus-details.component';
-import {WarcStatusPageComponent} from './warcstatus/warcstatus-page.component';
-import {WarcStatusService} from './warcstatus/warcstatus.service';
-import {SearchConfigComponent} from './search/search-config/search-config.component';
-import {DetailDirective} from './shared/detail.directive';
+
+import {
+  WarcStatusListComponent,
+  WarcStatusDetailsComponent,
+  WarcStatusPageComponent,
+  WarcStatusService
+} from './warcstatus';
+
 
 @NgModule({
   declarations: [
