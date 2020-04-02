ba0d4de6afe2c7ad8ee518625a0e7eace49d2851
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2020-04-02 18:28:21 +0200

    Add pagelog viewmodels

--- src/shared/models/report/index.ts
+++ src/shared/models/report/index.ts
@@ -2,3 +2,4 @@ export {ApiError} from './api-error.model';
 export {CrawlExecutionState, CrawlExecutionStatus, crawlExecutionStates} from './crawlexecutionstatus.model';
 export {JobExecutionState, JobExecutionStatus, jobExecutionStates} from './jobexecutionstatus.model';
 export {ExtraStatusCodes} from './extrastatuscodes.model';
+export {PageLog} from './pagelog.model';
