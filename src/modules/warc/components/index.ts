fa75f5c6f0fccfef724d5a5526b95ddb5d4fa479
Andreas Børsheim <andreas.borsheim@nb.no>
2020-10-23 15:29:03 +0200

    Remove warc report module

--- src/modules/warc/components/index.ts
+++
@@ -1,3 +0,0 @@
-export {WarcStatusListComponent} from './warcstatus-list/warcstatus-list.component';
-export {WarcStatusDetailsComponent} from './warcstatus-details/warcstatus-details.component';
-export {WarcStatusSummaryComponent} from './warcstatus-summary/warcstatus-summary.component';
