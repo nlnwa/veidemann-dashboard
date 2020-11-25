e3c3800add3d6920e008ce73fd3b71d5a695d2b2
Andreas Børsheim <andreasbo@localhost.localdomain>
2020-11-25 10:40:48 +0100

    Display health check in home page

--- src/modules/app/components/home/home.component.ts
+++ src/modules/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import {ControllerApiService, ErrorService} from '../../../core/services';
 import {CrawlerStatusDialogComponent} from '..';
 import {MatDialog} from '@angular/material/dialog';
 import {JobExecutionStatus} from '../../../../shared/models/report';
+import {Health, HealthApiService} from '../../../core/services/api/health-api.service';
 
 @Component({
   selector: 'app-home',
@@ -15,12 +16,16 @@ import {JobExecutionStatus} from '../../../../shared/models/report';
 export class HomeComponent implements OnInit {
 
   updateRunStatus: Subject<void> = new Subject();
+  updateHealthStatus: Subject<void> = new Subject();
   runStatus$: Observable<RunStatus>;
   runningCrawls$: Observable<JobExecutionStatus>;
+  healthStatus: Health;
+  healthStatus$: Observable<Health>;
 
   constructor(private errorService: ErrorService,
               private controllerApiService: ControllerApiService,
-              private dialog: MatDialog) {
+              private dialog: MatDialog,
+              private healthApiService: HealthApiService) {
   }
 
   ngOnInit(): void {
@@ -31,6 +36,16 @@ export class HomeComponent implements OnInit {
         return throwError(error);
       })
     );
+    this.healthStatus$ = merge(this.updateHealthStatus, timer(0, 30000)).pipe(
+      mergeMap(() => this.healthApiService.get()),
+      catchError(error => {
+        this.errorService.dispatch(error);
+        return throwError(error);
+      })
+    );
+
+    // this.showHealth();
+
   }
 
   onChangeRunStatus(shouldPause: boolean) {
@@ -48,6 +63,11 @@ export class HomeComponent implements OnInit {
         this.updateRunStatus.next();
       }
     });
+  }
+
+  showHealth() {
+    this.healthApiService.get()
+      .subscribe((data: Health) => this.healthStatus = { ...data});
   }
 
 }
