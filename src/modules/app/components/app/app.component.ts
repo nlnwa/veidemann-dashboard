39a36555e2bcb0416ad1417f6b914d9cb3158c91
Andreas Børsheim <andreas.borsheim@nb.no>
2021-03-16 16:45:57 +0100

    Enable keyboard route shortcuts from everywehere

--- src/modules/app/components/app/app.component.ts
+++ src/modules/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {ActivatedRoute, RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterEvent} from '@angular/router';
 
 import {Observable} from 'rxjs';
@@ -7,6 +7,7 @@ import {AppInitializerService, ControllerApiService, ErrorService, SnackBarServi
 import {AuthService, GuardService} from '../../../core/services/auth';
 import {MatDialog} from '@angular/material/dialog';
 import {AboutDialogComponent} from '../about-dialog/about-dialog.component';
+import {ShortcutEventOutput, ShortcutInput} from 'ng-keyboard-shortcuts';
 
 @Component({
   selector: 'app-root',
@@ -14,10 +15,11 @@ import {AboutDialogComponent} from '../about-dialog/about-dialog.component';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit{
 
   isModuleLoading$: Observable<boolean>;
   private moduleLoadSemaphore = 0;
+  shortcuts: ShortcutInput[] = [];
 
   constructor(private appInitializer: AppInitializerService,
               private authService: AuthService,
@@ -59,6 +61,101 @@ export class AppComponent implements OnInit {
       }
     }
 
+  }
+
+  ngAfterViewInit(): void {
+    this.shortcuts.push(
+      {
+        key: 'c e',
+        label: 'Configurations',
+        description: 'Show entity view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/entity'])
+      },
+      {
+        key: 'c s',
+        label: 'Configurations',
+        description: 'Show seed view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/seed'])
+      },
+      {
+        key: 'c j',
+        label: 'Configurations',
+        description: 'Show crawljob view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/crawljobs'])
+      },
+      {
+        key: 'c s c',
+        label: 'Configurations',
+        description: 'Show schedule view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/schedule'])
+      },
+      {
+        key: 'c r',
+        label: 'Configurations',
+        description: 'Show crawlconfig view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/crawlconfig'])
+      },
+      {
+        key: 'c o',
+        label: 'Configurations',
+        description: 'Show collection view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/collection'])
+      },
+      {
+        key: 'c b',
+        label: 'Configurations',
+        description: 'Show browserconfig view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/browserconfig'])
+      },
+      {
+        key: 'c b s',
+        label: 'Configurations',
+        description: 'Show browserscript view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/browserscript'])
+      },
+      {
+        key: 'c p',
+        label: 'Configurations',
+        description: 'Show politeness view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/politenessconfig'])
+      },
+      {
+        key: 'c h',
+        label: 'Configurations',
+        description: 'Show crawlhostgroup view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/crawlhostgroupconfig'])
+      },
+      {
+        key: 'c u',
+        label: 'Configurations',
+        description: 'Show users view',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/config/rolemapping'])
+      },
+      {
+        key: 'r j',
+        label: 'Reports',
+        description: 'Show jobexecution reports',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/report/jobexecution'])
+      },
+      {
+        key: 'r c',
+        label: 'Reports',
+        description: 'Show crawlexecution reports',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/report/jobexecution'])
+      },
+      {
+        key: 'r p',
+        label: 'Reports',
+        description: 'Show pagelog reports',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/report/jobexecution'])
+      },
+      {
+        key: 'r c l',
+        label: 'Reports',
+        description: 'Show crawllog reports',
+        command: (output: ShortcutEventOutput) => this.router.navigate(['/report/jobexecution'])
+      }
+    );
   }
 
   get error(): Error {
