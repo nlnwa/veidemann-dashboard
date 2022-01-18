4d72814d0bb921891b9b92250b1b300e12d780c6
Andreas Børsheim <andreas.borsheim@nb.no>
2022-01-18 15:13:21 +0100

    fix tests

--- src/modules/config/components/run-crawl-dialog/run-crawl-dialog.component.spec.ts
+++ src/modules/config/components/run-crawl-dialog/run-crawl-dialog.component.spec.ts
@@ -1,49 +1,214 @@
 import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
 
 import {RunCrawlDialogComponent} from './run-crawl-dialog.component';
-import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
-import {ConfigObject, Kind} from '../../../../shared/models';
-import {MatSelectModule} from '@angular/material/select';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import {ConfigObject, CrawlJob, Kind, Meta} from '../../../../shared/models';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModel} from '@angular/forms';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import {expect} from '@angular/flex-layout/_private-utils/testing';
+import {CommonsModule} from '../../../commons';
+import {MaterialModule} from '../../../commons/material.module';
+import {CoreTestingModule} from '../../../core/core.testing.module';
+import {MatButtonHarness} from '@angular/material/button/testing';
+import {MatSelectHarness} from '@angular/material/select/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
+
+const exampleCrawljobs = [
+  {
+    id: 'configObject_id',
+    apiVersion: 'v1',
+    kind: Kind.CRAWLJOB,
+    meta: new Meta({
+      name: 'Example CrawlJob',
+    }),
+    crawlJob: new CrawlJob({}),
+    disabled: false
+  },
+  {
+    id: 'configObject_id2',
+    apiVersion: 'v1',
+    kind: Kind.CRAWLJOB,
+    meta: new Meta({
+      name: 'Example CrawlJob2',
+    }),
+    crawlJob: new CrawlJob({}),
+    disabled: false
+  },
+];
+
+const exampleCrawljobToCrawl = {
+  runCrawlReply: {
+    jobExecutionId: 'testid'
+  },
+  configObject: new ConfigObject({
+    id: 'test_crawljob_id',
+    apiVersion: 'v1',
+    kind: Kind.CRAWLJOB,
+    meta: new Meta({
+      name: 'Example Crawljob'
+    }),
+    crawlJob: new CrawlJob({
+    }),
+  }),
+  crawlJobs: exampleCrawljobs,
+};
+
+const exampleSeedToCrawl = {
+  kind: Kind.SEED,
+  runCrawlReply: {
+    jobExecutionId: 'testid'
+  },
+  configObject: new ConfigObject({
+    id: 'test_seed_id',
+    apiVersion: 'v1',
+    kind: Kind.SEED,
+    meta: new Meta({
+      name: 'https://www.nb.no'
+    })
+  }),
+  crawlJobs: exampleCrawljobs
+};
+
+const exampleSeedsToCrawl = {
+  ...exampleSeedToCrawl,
+  numberOfSeeds: 3
+};
+
+// TODO: Test that RunCrawlRequest that is returned in onRunCrawl contains correct data,
 
 describe('RunCrawlDialogComponent', () => {
   let component: RunCrawlDialogComponent;
   let fixture: ComponentFixture<RunCrawlDialogComponent>;
+  let loader: HarnessLoader;
+
+  let runButton: MatButtonHarness;
+  let cancelButton: MatButtonHarness;
+
+  let crawljobSelect: MatSelectHarness;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [MatSelectModule, NoopAnimationsModule],
-      declarations: [ RunCrawlDialogComponent, NgModel ],
+      imports: [
+        MatDialogModule,
+        MaterialModule,
+        RouterTestingModule,
+        CoreTestingModule.forRoot(),
+        CommonsModule,
+        NoopAnimationsModule
+      ],
+      declarations: [
+        RunCrawlDialogComponent
+      ],
       providers: [
         {provide: MatDialog, useValue: {}},
         {provide: MatDialogRef, useValue: {}},
-        {provide: MAT_DIALOG_DATA, useValue: {}}
       ]
-  })
-    .compileComponents();
+    })
+      .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RunCrawlDialogComponent);
-    component = fixture.componentInstance;
-    component.configObject = new ConfigObject();
-    fixture.detectChanges();
+  describe('Run crawl dialog for start crawling a crawljob', async () => {
+    beforeEach(async () => {
+      TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: exampleCrawljobToCrawl});
+      fixture = TestBed.createComponent(RunCrawlDialogComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      runButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'RUN'}));
+      cancelButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'CANCEL'}));
+    });
+
+    it('should display correct name of crawljob to start crawling', () => {
+      expect(fixture.debugElement.query(
+        By.css('[data-testid="run_crawljob_dialog_text"]')).nativeElement.innerText)
+        .toEqual('Are you sure you want to start the crawljob: Example Crawljob?');
+    });
+
+    it('should call onRunCrawl when run button is clicked', async () => {
+      const onRunCrawlMock = spyOn(component, 'onRunCrawl');
+      expect(await runButton.isDisabled()).toBeFalsy();
+      await runButton.click();
+      expect(onRunCrawlMock).toHaveBeenCalled();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Run crawl dialog for start crawling a single seed', async () => {
+    beforeEach(async () => {
+      TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: exampleSeedToCrawl});
+      fixture = TestBed.createComponent(RunCrawlDialogComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+
+      cancelButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'CANCEL'}));
+      crawljobSelect = await loader.getHarness(MatSelectHarness);
+
+    });
+
+    it('should display correct name of seed to start crawling', () => {
+      expect(fixture.debugElement.query(
+        By.css('[data-testid="run_seed_dialog_text"]')).nativeElement.innerText)
+        .toEqual('Are you sure you want to start crawling: https://www.nb.no?');
+    });
+
+    it('Run button should be active if crawljob is selected', async () => {
+      expect(fixture.debugElement.query(By.css('[data-testid="runCrawlButton"]'))).toBeNull();
+      await crawljobSelect.open();
+      const options = await crawljobSelect.getOptions();
+      await options[0].click();
+      expect(component.jobRefId).toEqual('configObject_id');
+      runButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'RUN'}));
+      expect(await runButton.isDisabled()).toBeFalsy();
+    });
+
+    it('should call onRunCrawl when run button is clicked', async () => {
+      const onRunCrawlMock = spyOn(component, 'onRunCrawl');
+      await crawljobSelect.open();
+      const options = await crawljobSelect.getOptions();
+      await options[0].click();
+      runButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'RUN'}));
+      expect(await runButton.isDisabled()).toBeFalsy();
+      await runButton.click();
+      expect(onRunCrawlMock).toHaveBeenCalled();
+    });
   });
 
-  it('should run from Seed', () => {
-    component.configObject = new ConfigObject({kind: Kind.SEED});
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
-  });
+  describe('Run crawl dialog for start crawling multiple seeds', async () => {
+    beforeEach(async () => {
+      TestBed.overrideProvider(MAT_DIALOG_DATA, {useValue: exampleSeedsToCrawl});
+      fixture = TestBed.createComponent(RunCrawlDialogComponent);
+      loader = TestbedHarnessEnvironment.loader(fixture);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
 
-  it('should run from CrawlJob', () => {
-    component.configObject = new ConfigObject({kind: Kind.CRAWLJOB});
-    fixture.detectChanges();
-    expect(component).toBeTruthy();
+      cancelButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'CANCEL'}));
+      crawljobSelect = await loader.getHarness(MatSelectHarness);
+    });
+
+
+    it('should display correct number of seeds to start crawling', () => {
+      expect(fixture.debugElement.query(
+        By.css('[data-testid="run_multiple_seeds_dialog_text"]')).nativeElement.innerText)
+        .toEqual('Are you sure you want to start crawling 3 seeds?');
+    });
+
+    it('List of crawljobs to use for crawling the seeds contains all options: ', async () => {
+      await crawljobSelect.open();
+      const options = await crawljobSelect.getOptions();
+      expect(options.length).toEqual(2);
+    });
+
+    it('Run button should be active if crawljob is selected', async () => {
+      expect(fixture.debugElement.query(By.css('[data-testid="runCrawlButton"]'))).toBeNull();
+      await crawljobSelect.open();
+      const options = await crawljobSelect.getOptions();
+      await options[0].click();
+      expect(component.jobRefId).toEqual('configObject_id');
+      runButton = await loader.getHarness<MatButtonHarness>(MatButtonHarness.with({text: 'RUN'}));
+      expect(await runButton.isDisabled()).toBeFalsy();
+    });
   });
 });
