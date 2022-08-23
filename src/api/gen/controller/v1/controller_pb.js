25e3f4baa5bea4620c18465ba7a6fd5a4d9f10ac
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2022-08-23 12:33:52 +0200

    Regenerate API

--- src/api/gen/controller/v1/controller_pb.js
+++ src/api/gen/controller/v1/controller_pb.js
@@ -13,13 +13,7 @@
 
 var jspb = require('google-protobuf');
 var goog = jspb;
-var global = (function() {
-  if (this) { return this; }
-  if (typeof window !== 'undefined') { return window; }
-  if (typeof global !== 'undefined') { return global; }
-  if (typeof self !== 'undefined') { return self; }
-  return Function('return this')();
-}.call(null));
+var global = (function() { return this || window || global || self || Function('return this')(); }).call(null);
 
 var controller_v1_resources_pb = require('../../controller/v1/resources_pb.js');
 goog.object.extend(proto, controller_v1_resources_pb);
